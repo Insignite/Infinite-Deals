bcad5597d2db8d8dbc4fa72435c978cbaa485962
Khanh Nguyen <nh.minhkhanhLA@gmail.com>
2018-04-25 18:26:26 -0700

    Extra main files

---
+++ JavaScript/googleCSE.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '011139275606927959748:wlk5jjddrea';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
---
+++ WebCrawler/crawl.php
@@ -0,0 +1,6 @@
+'''Database UML - 4-5pm
+341 Lab - 5-6pm
+Study for 341 - 6-7:30pm
+Meal - 7:30-8:30pm
+343 - 8:30pm-midnight
+'''
